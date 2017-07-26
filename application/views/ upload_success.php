2befee654ab5aca3e3431a4875d0f5cb8712d805
Boris Terraza G <ingterraza@outlook.com>
2017-07-26 17:36:51 -0500

    Vista Confirmacion
    Vista Confirmacion

--- application/views/ upload_success.php
+++ application/views/ upload_success.php
@@ -2,16 +2,55 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html>
-    <head>
-        <title>Upload Form</title>
+    <head lang="es">
+        <title>Welcome to CodeIgniter</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
     </head>
     <body>
-        <h3>Your file was successfully uploaded!</h3>
-        <ul>
-            <?php foreach ($upload_data as $item => $value): ?>
-                <li><?php echo $item; ?>: <?php echo $value; ?></li>
-            <?php endforeach; ?>
-        </ul>
-        <p><?php echo anchor('upload', 'Upload Another File!'); ?></p>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8"> 
+                    <div class="text-info">
+                        <h2>Your file was successfully uploaded!...</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Info File</h3>
+                        </div>
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <ul>
+                                        <?php foreach ($upload_data as $item => $value): ?>
+                                            <li><?php echo $item; ?>: <?php echo $value; ?></li>
+                                        <?php endforeach; ?>
+                                    </ul>
+                                </div>
+                                <div class="col-md-3">
+                                    <p><?php echo anchor('index.php/upload', 'Upload Another File!'); ?></p>
+                                </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
     </body>
 </html>
