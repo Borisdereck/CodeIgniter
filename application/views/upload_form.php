c169b5b3a0dde4cf05991910292e52a5230380e5
Boris Terraza G <ingterraza@outlook.com>
2017-07-26 17:23:20 -0500

    Subir Imagen
    Cambio a los estilos del index de subir los archivos

--- application/views/upload_form.php
+++ application/views/upload_form.php
@@ -3,15 +3,48 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html>
     <head>
-        <title>Upload Form</title>
+        <meta charset="utf-8">
+        <title>Welcome to CodeIgniter</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
     </head>
     <body>
-        <?php echo $error; ?>
-        <?php echo form_open_multipart('index.php/upload/do_upload'); ?>
-        <input type="file" name="userfile" size="20" />
-        <br /><br />
-        <input type="submit" value="upload" />
-    </form>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8"> 
+                    <div class="text-info">
+                        <h2>Coding...</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Upload File</h3>
+                        </div>
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <?php echo $error; ?>
+                                    <?php echo form_open_multipart('index.php/upload/do_upload'); ?>
+                                    <input type="file"  name="userfile" size="20" />
+                                </div>
+                                <div class="col-md-3">
+                                    <input type="submit" class="btn btn-default" value="upload!" />
+                                </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-</body>
+    </body>
 </html>
